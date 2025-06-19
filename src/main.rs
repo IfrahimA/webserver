edb6107d47dd62bf3820643bcee9bc44de547019
Ifrahim Ansari <147778089+IfrahimA@users.noreply.github.com>
2025-06-18 23:10:06 -0400

    library to implement multithreading

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,43 @@ use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+use webserver::ThreadPool;
 
 fn main() {
-    //* TCP LISTENER: Listens for incoming connections */
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    //* ITERATE: ASYNC */
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        //* HANDLE CONNECTIONS */
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    //* Wraps around stream, manages the stream by applying a buffer.
     let buf_reader = BufReader::new(&stream);
-
-    //* HTTP Request -> First Line */
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    //* Request Header */
-    //* METHOD - ROUTE - HTML VERSION - CRLF */
-    let (statusline, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "./src/sample.html")
-    } else {
-        ("HTTP/1.1 400 NOT FOUND", "./src/404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    //* Response Header */
-    //* HTML VERSION - STATUS CODE - PHRASE - CRLF */
-    let contents: String = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-    let response = format!("{statusline}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
     stream.write_all(response.as_bytes()).unwrap();
 }
